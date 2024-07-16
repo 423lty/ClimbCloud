d466afa92ebda497ed7a8c3ae04f6a3a73645911
haz <hzk770801@gmail.com>
2024-07-16 22:37:18 +0900

    24/07/16 キャラアニメーション歩行実装

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Rigidbody
+    Rigidbody2D rigidbody2D;
+
+    Animator animation;
+
+    //力
+    float jumpForce = 680f;
+
+    float walkForce = 30f;
+
+    float maxWalkSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //フレーム買う
+        Application.targetFrameRate = 60;
+
+        this.rigidbody2D = GetComponent<Rigidbody2D>();
+        this.animation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Jump();
+
+        InputArrow();
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            this.rigidbody2D.AddForce(transform.up * this.jumpForce);
+        }
+
+    }
+    /// <summary>
+    /// 移動
+    /// </summary>
+    void InputArrow()
+    {
+        int key = 0;
+        //左右
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            key = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            key = -1;
+        }
+
+        //スピード
+        float speed = Mathf.Abs(this.rigidbody2D.velocity.x);
+
+        if (speed < this.maxWalkSpeed)
+        {
+            this.rigidbody2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        //反転
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        this.animation.speed = speed / 0.75f;
+    }
+
+}
